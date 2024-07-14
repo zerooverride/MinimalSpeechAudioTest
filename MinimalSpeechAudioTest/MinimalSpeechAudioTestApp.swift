a818bc1b6de4e83ac42eead9009c8daaa0ff90b7
Tyler Eckstein <tyler.s.eckstein@gmail.com>
2024-07-13 21:01:30 -0400

    Initial Commit

---
+++ MinimalSpeechAudioTest/MinimalSpeechAudioTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalSpeechAudioTestApp.swift
+//  MinimalSpeechAudioTest
+//
+//  Created by Tyler Eckstein on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MinimalSpeechAudioTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
