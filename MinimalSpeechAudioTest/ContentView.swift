6979745b8af3399a4518b2ea47233c265100489d
Tyler Eckstein <tyler.s.eckstein@gmail.com>
2024-07-30 22:46:37 -0400

    Some minor improvements but not a full solution, still waiting for response from Apple.
    1. Removes UI pause when deativating session by putting it in a background queue,
    	but it still throws and exception
    2. UI still pauses during the first use of a sythesizer after app launch
    3. Added an exmaple that plays a pre-recorded audio working all as expected.

--- MinimalSpeechAudioTest/ContentView.swift
+++ MinimalSpeechAudioTest/ContentView.swift
@@ -23,8 +23,9 @@ class Rotator {
 struct ContentView: View {
     let workingSynthesizer = UnduckingSpeechSynthesizer()
     let brokenSynthesizer = BrokenSpeechSynthesizer()
+    let customAudioPlayer = CustomAudioPlayer()
     let rotator = Rotator()
-    
+
     init() {
         let audioSession = AVAudioSession.sharedInstance()
         do {
@@ -37,12 +38,16 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Button("Un-ducks Correctly. But throws exception and pauses UI."){
+            Button("Un-ducks Correctly. But throws exception. First synthesizer usage after app launch pauses UI."){
                 workingSynthesizer.speak(text: "Hello planet")
             }
             Text("-------")
-            Button("Does not un-duck other audio."){
+            Button("Does not un-duck other audio. First synthesizer usage after app launch pauses UI."){
                 brokenSynthesizer.speak(text: "Hello planet")
+            }
+            Text("-------")
+            Button("Pre-made Audio File."){
+                customAudioPlayer.play(soundfile: "Hello_planet_1")
             }
             Text("-------")
             Label("Arrow", systemImage: "arrow.up").rotationEffect(.degrees(rotator.rotation)).labelStyle(.iconOnly)
@@ -68,24 +73,67 @@ class UnduckingSpeechSynthesizer: NSObject {
 
 extension UnduckingSpeechSynthesizer: AVSpeechSynthesizerDelegate {
     func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
-        do {
-            try audioSession.setActive(false, options: .notifyOthersOnDeactivation)
-        }
-        catch {
-            // always throws an error
-            // Error Domain=NSOSStatusErrorDomain Code=560030580 "Session deactivation failed" UserInfo={NSLocalizedDescription=Session deactivation failed}
-            // Also pauses UI updates
-            print("Deactivate error info: \(error)")
+        // putting to any higher qos makes UI pause during audio session deactivation
+        DispatchQueue.global(qos: .utility).async {
+            do {
+                try self.audioSession.setActive(false, options: .notifyOthersOnDeactivation)
+            }
+            catch {
+                // always throws an error
+                // Error Domain=NSOSStatusErrorDomain Code=560030580 "Session deactivation failed" UserInfo={NSLocalizedDescription=Session deactivation failed}
+                // Also pauses UI updates
+                print("Deactivate error info: \(error)")
+            }
         }
     }
 }
 
 class BrokenSpeechSynthesizer {
     var synth = AVSpeechSynthesizer()
-    
+    let audioSession = AVAudioSession.sharedInstance()
+
     func speak(text: String) {
         let utterance = AVSpeechUtterance(string: text)
-        synth.speak(utterance)
+        self.synth.speak(utterance)
+    }
+}
+
+class CustomAudioPlayer {
+    var audioPlayer: AVAudioPlayer?
+    let audioSession = AVAudioSession.sharedInstance()
+
+    func play(soundfile: String) {
+        // putting to any higher qos makes UI pause during audio session deactivation
+        DispatchQueue.global(qos: .utility).async {
+            if let path = Bundle.main.path(forResource: soundfile, ofType: "wav"){
+                do {
+                    // doesn't seem necesssary but seems to be best practice
+                    try self.audioSession.setActive(true)
+                }
+                catch {
+                    print("Activate error info: \(error)")
+                }
+                do{
+                    self.audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+                    self.audioPlayer?.prepareToPlay()
+                    self.audioPlayer?.play()
+                } catch {
+                    print("Error")
+                }
+                
+                while(self.audioPlayer!.isPlaying){
+                    // wait so audio isn't stopped pre-maturely
+                }
+                do {
+                    try self.audioSession.setActive(false, options: .notifyOthersOnDeactivation)
+                }
+                catch {
+                    print("Deactivate error info: \(error)")
+                }
+            } else {
+                print("No file")
+            }
+        }
     }
 }
 
